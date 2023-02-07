e6e1303c53dd50f07cbc3a66fb5757fab0351568
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2023-02-07 13:02:51 +0600

    Initial commit

---
+++ src/main/java/com/tonmoy/test1/test1Application.java
@@ -0,0 +1,14 @@
+package com.tonmoy.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class test1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(test1Application.class, args);
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/com/tonmoy/test1/Tonmoy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonmoy.test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tonmoy1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
